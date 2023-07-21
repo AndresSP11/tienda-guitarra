e8f3f0c8b6aacce7e182eceae61f49ae1faa2139
Anderson <andres.salazar.p@uni.pe>
2023-07-21 11:15:48 -0500

    Origin main

---
+++ guitarLA/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const titulo="Bienvenidos al Mundo de Vue Js"
+</script>
+
+<template>
+  <h1>{{ titulo }}</h1>
+  
+</template>
+
+<style scoped>
+  h1{
+    color: blue;
+    font-size: 50px;
+  }
+</style>
